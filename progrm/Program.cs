ec4ffaffcf275af410606d4e871f488d3ce247c5
v.tarasov <v.tarasov163@gmail.com>
2022-10-02 22:17:37 +0400

    Добавил решение

---
+++ progrm/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string[] array1 = new string[6] {"hello", "world", "1", "22", "333", "4444"};
+string[] array2 = new string[array1.Length];
+
+int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+Console.WriteLine($"первый массив: [{string.Join(" ", array1)}]");
+Console.WriteLine($"второй массив: [{string.Join(", ", array2)}]");
